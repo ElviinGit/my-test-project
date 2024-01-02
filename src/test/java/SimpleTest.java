7555a989759222ce87c6195323455fba5b28c5db
ElviinGit <elvnmlkvxr1@icloud.com>
2024-01-02 13:27:34 +0400

    initial project files

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SimpleTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testSubtaction() {
+        assertEquals(5, 10 - 5);
+    }
+}
